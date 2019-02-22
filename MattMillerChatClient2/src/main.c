c9f1bf018f49f9961564d778ff3ef071f56c4d1c
Pablo-Pigcasso <mattmiller@westmont.edu>
2019-02-21 16:14:03 -0800

    reordered and cleaned original code from before today

--- MattMillerChatClient2/src/main.c
+++ MattMillerChatClient2/src/main.c
@@ -13,169 +13,108 @@
  */
 
 #include <stdio.h>
-#include <time.h> //from class notes
-#include <sys/select.h> //from class notes
-#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/types.h>
-#include <sys/socket.h> //sockets
-#include <unistd.h> //write
-#include <errno.h> //errors
-#include <string.h> //strings and info
+#include <arpa/inet.h>
+#include <errno.h>
+#include <time.h>
 
 //NEW CODE
-#define SERVER "10.115.20.250"
-#define PORT 49153
+#define S_IP "10.115.20.250"
+#define S_PORT 49153
 #define BUFSIZE 1024
-#define MESSAGE_S 80
 
-//Intial declarations
-//struct sockaddr_in server; //server is variable name of Rodkey's server
-struct timeval timev; //timeval is going help time for the while loop
-int descriptor, fd, len;
-int is_done = 0;
-char *name, *buffer, *origbuffer; //setting size of name, message, and reply
-//fds from github: https://github.com/dtolj/simple-chat-client-server/blob/master/chat.c
-fd_set readfds, testfds, clientfds;
-int sockfd, result;
-char msg[MESSAGE_S + 1];
-char kb_msg[MESSAGE_S + 10];
-
-
-//new function for connecting to the server
 int connect2v4stream(char * srv, int port){
     int ret,sockd;
     struct sockaddr_in sin;
     
-    if ((sockd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-        printf("ERROR: Socket failed to be created correctly. errno = %d\n", errno);
-        exit(errno);
-    }
-    if ((ret = inet_pton(AF_INET, SERVER, &sin.sin_addr)) <= 0){
-        printf("ERROR: Trouble converting using inet_pton. return value = %d, errno = %d\n", ret, errno);
+    if((sockd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        printf("ERROR: Could not create socket. errno = %d\n", errno);
         exit(errno);
     }
     
-    sin.sin_family = AF_INET; //Used for IPV4
-    sin.sin_port = htons(PORT); //Convert port to network endian
+    if((ret = inet_pton(AF_INET, S_IP, &sin.sin_addr)) <= 0){
+        printf("ERROR: could not correctly convert using inet_pton. \ return value = %d, errno = %d\n", ret, errno);
+        exit(errno);
+    }
+    
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(S_PORT);
     
     if((connect(sockd, (struct sockaddr *) &sin, sizeof(sin))) == -1){
-        printf("ERROR: Could not connect to the server. errno = %d\n", errno);
+        printf("ERROR: trouble connecting to server. errno = %d\n", errno);
         exit(errno);
     }
     return sockd;
 }
 
-//new function to replace send
 int sendout(int fd, char *msg){
-    
     int ret;
     ret = send(fd, msg, strlen(msg), 0);
     if(ret == -1){
-        printf("ERROR: Trouble sending. errno = %d\n", errno);
+        printf("ERROR: trouble sending. errno = %d\n", errno);
         exit(errno);
     }
     return strlen(msg);
 }
 
-// new receive and print, it grabs all strings from server until there are no more.
-void recvandprint (int fd, char *buff){
-    
+void recvandprint(int fd, char *buff){
     int ret;
-    
     for(;;){
         buff = malloc(BUFSIZE+1);
-        ret = recv(fd, buff, BUFSIZE, 0);
-        if(ret == -1){
+        ret = recv(fd,buff,BUFSIZE,0);
+        if(ret==-1){
             if(errno == EAGAIN){
                 break;
             } else {
-                printf("ERROR: error receiving from server. errno = %d\n", errno);
+                printf("ERROR: error receiving. errno = %d\n", errno);
                 exit(errno);
             }
-        } else if (ret == 0) {
+        } else if (ret == 0){
             exit(0);
         } else {
             buff[ret] = 0;
-            printf("%s", buff);
+            printf("%s",buff);
         }
         free(buff);
     }
 }
 
-// lets start with something we know... main
 int main(int argc, char * argv[]){
+    int fd, len;
+    char *name, *buffer, *origbuffer;
+    struct timeval timev;
     
-    //NEW CONNECT VIA IN CLASS NOTES
-    fd = connect2v4stream( SERVER, PORT);
+    fd = connect2v4stream(S_IP, S_PORT);
     
-    //Timeout timer, set to half a second
     timev.tv_sec = 0;
     timev.tv_usec = 1000 * 500;
     setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, &timev, sizeof(timev));
     
-    
     if(argc < 2){
-        printf("Usage: chat-client Username\n");
+        printf("Usage: Networks chat-client username\n");
         exit(1);
     }
     name = argv[1];
     len = strlen(name);
-    name[len] = '\n'; //inserts line break
-    name[len+1] = '\0'; //terminates the string
+    name[len] = '\n';
+    name[len+1] = '\0';
     sendout(fd, name);
     
-    //TODO: cite github
-    FD_ZERO(&clientfds);
-    FD_SET(sockfd,&clientfds);
-    FD_SET(0,&clientfds);//stdin
     
-    //TODO: Dissect this code.
-    while(1) {
-        testfds = clientfds;
-        select(FD_SETSIZE, &testfds, NULL, NULL, NULL);
+    int is_done = 0;
+    while(!is_done){
+        recvandprint(fd,buffer);
         
-        for(fd=0;fd<FD_SETSIZE;fd++){
-            if(FD_ISSET(fd,&testfds)){
-                if(fd==sockfd){
-                    result = read(sockfd, msg, MESSAGE_S);
-                    msg[result] = '\0';
-                    printf("%s", msg + 1);
-                    
-                    if(msg[0] == 'X'){
-                        close(sockfd);
-                        exit(0);
-                    }
-                    
-                }
-                else if(fd == 0){
-                    
-                    fgets(kb_msg, MESSAGE_S+1, stdin);
-                    if (strcmp(kb_msg, "quit\n") == 0){
-                        sprintf(msg, "Disconnecting, shutting down.\n");
-                        write(sockfd, msg, strlen(msg));
-                        close(sockfd);
-                        exit(0);
-                    }
-                    else {
-                        sprintf(msg, "M%s", kb_msg);
-                        write(sockfd, msg, strlen(msg));
-                    }
-                }
-            }
+        len = BUFSIZE;
+        buffer = malloc(len+1);
+        origbuffer = buffer;
+        if(getline(&buffer,(size_t *) &len,stdin) > 1){
+            sendout(fd, buffer);
         }
+        is_done = (strcmp (buffer, "quit\n") == 0);
+        free(origbuffer);
     }
-    /*    while(!is_done){
-     recvandprint(fd, buffer);
-     len = BUFSIZE;
-     buffer = malloc(len+1);
-     origbuffer = buffer;
-     if(getline(&buffer,(size_t *) &len,stdin) > 1){
-     sendout(fd, buffer);
-     }
-     is_done = (strcmp (buffer, "quit\n") == 0);
-     free(origbuffer);
-     }
-     
-     */
 }
